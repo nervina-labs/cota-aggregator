23bb7204a69f7b369aa3953a12662c56712efc24
duanyytop <duanyytop@gmail.com>
2021-12-02 16:26:45 +0800

    feat: Add jsonrpc server

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # compact-nft-aggregator
+
+The aggregator of compact-NFT service
+
+### Quick Start
+```shell
+cargo build
+
+cargo run
+
+cargo test
+```
+
+### Usage
+
+```shell
+echo '{
+    "id": 2,
+    "jsonrpc": "2.0",
+    "method": "generate_mint_compact_nft",
+    "params": ["0xea28c98f38b4a57aa81756b167bb37fa42daf67edbc9863afb8172096ed301c2"]
+}' \
+| tr -d '\n' \
+| curl -H 'content-type: application/json' -d @- \
+http://localhost:3030
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+use crate::utils::check_request_params;
+use jsonrpc_http_server::jsonrpc_core::serde_json::Map;
+use jsonrpc_http_server::jsonrpc_core::{IoHandler, Params, Value};
+use jsonrpc_http_server::ServerBuilder;
+
+mod utils;
+
 fn main() {
-    println!("Hello, world!");
+    let mut io = IoHandler::default();
+    io.add_method("generate_mint_compact_nft", |params: Params| async move {
+        match params {
+            Params::Array(values) => {
+                println!("params: {:?}", values);
+                if let Some(error) = check_request_params(values.clone()) {
+                    return Ok(error);
+                }
+
+                let mut response = Map::new();
+                response.insert(
+                    "registry_entries".to_string(),
+                    Value::String("generate_mint_compact_nft".to_string()),
+                );
+                response.insert(
+                    "smt_root_hash".to_string(),
+                    Value::String("smt_root_hash".to_string()),
+                );
+
+                Ok(Value::Object(response))
+            }
+            _ => Ok(Value::String("Request parameter format error".to_owned())),
+        }
+    });
+
+    let server = ServerBuilder::new(io)
+        .threads(3)
+        .start_http(&"127.0.0.1:3030".parse().unwrap())
+        .unwrap();
+
+    server.wait();
 }
