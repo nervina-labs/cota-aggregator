2bd0619157dbb1a9d35b8dc21de618fe658961a2
duanyytop <duanyytop@gmail.com>
2021-12-24 21:30:50 +0800

    feat: Update request parameter data structure

--- rpc/src/request/mint.rs
+++ rpc/src/request/mint.rs
@@ -1,15 +1,14 @@
 use crate::error::Error;
-use crate::utils::HexParser;
+use crate::utils::{parse_bytes, HexParser};
 use jsonrpc_http_server::jsonrpc_core::serde_json::Map;
 use jsonrpc_http_server::jsonrpc_core::Value;
 
-#[derive(Copy, Clone, Eq, PartialEq)]
+#[derive(Clone, Eq, PartialEq)]
 pub struct MintWithdrawal {
     pub token_index:    [u8; 4],
-    pub configure:      u8,
     pub state:          u8,
     pub characteristic: [u8; 20],
-    pub to_lock_hash:   [u8; 32],
+    pub to_lock_script: Vec<u8>,
 }
 
 impl MintWithdrawal {
@@ -18,23 +17,25 @@ impl MintWithdrawal {
         let mut token_index = [0u8; 4];
         token_index.copy_from_slice(&token_index_vec);
 
-        let configure = map.get_hex_bytes_filed("configure", 1)?[0];
         let state = map.get_hex_bytes_filed("state", 1)?[0];
 
         let characteristic_vec = map.get_hex_bytes_filed("characteristic", 20)?;
         let mut characteristic = [0u8; 20];
         characteristic.copy_from_slice(&characteristic_vec);
 
-        let to_lock_hash_vec = map.get_hex_bytes_filed("to_lock_hash", 32)?;
-        let mut to_lock_hash = [0u8; 32];
-        to_lock_hash.copy_from_slice(&to_lock_hash_vec);
+        let to_lock_script_value = map
+            .get("to_lock_script")
+            .ok_or(Error::RequestParamNotFound("to_lock_script".to_owned()))?;
+        if !to_lock_script_value.is_string() {
+            return Err(Error::RequestParamTypeError("to_lock_script".to_owned()));
+        }
+        let to_lock_script = parse_bytes(to_lock_script_value.as_str().unwrap().to_owned())?;
 
         Ok(MintWithdrawal {
             token_index,
-            configure,
             state,
             characteristic,
-            to_lock_hash,
+            to_lock_script,
         })
     }
 }
@@ -43,7 +44,7 @@ impl MintWithdrawal {
 pub struct MintReq {
     pub lock_hash:   [u8; 32],
     pub cota_id:     [u8; 20],
-    pub out_point:   [u8; 72],
+    pub out_point:   [u8; 36],
     pub withdrawals: Vec<MintWithdrawal>,
 }
 
@@ -57,8 +58,8 @@ impl MintReq {
         let mut cota_id = [0u8; 20];
         cota_id.copy_from_slice(&cota_id_vec);
 
-        let out_point_vec = map.get_hex_bytes_filed("out_point", 72)?;
-        let mut out_point = [0u8; 72];
+        let out_point_vec = map.get_hex_bytes_filed("out_point", 36)?;
+        let mut out_point = [0u8; 36];
         out_point.copy_from_slice(&out_point_vec);
 
         let withdrawals_value = map
--- rpc/src/request/mod.rs
+++ rpc/src/request/mod.rs
@@ -1,2 +1,2 @@
 pub(crate) mod define;
-mod mint;
+pub(crate) mod mint;
