0dd4a6e9cc43e62776b3bfd7d1c817badb18919b
duanyytop <duanyytop@gmail.com>
2022-01-25 19:51:51 +0800

    refactor: Clean code

--- rpc/src/db/mysql.rs
+++ rpc/src/db/mysql.rs
@@ -10,9 +10,9 @@ use log::error;
 use mysql::prelude::*;
 use mysql::*;
 use std::collections::HashMap;
+use std::env;
 use std::sync::MutexGuard;
 use std::{result::Result, sync::Mutex};
-use std::env;
 
 lazy_static! {
     pub static ref CONN: Mutex<PooledConn> = {
@@ -27,7 +27,8 @@ lazy_static! {
         } else {
             20
         };
-        let pool = Pool::new_manual(min, max, Opts::from_url(&url).unwrap()).expect("Database pool error");
+        let pool =
+            Pool::new_manual(min, max, Opts::from_url(&url).unwrap()).expect("Database pool error");
         Mutex::new(pool.get_conn().expect("Database connection error"))
     };
 }
