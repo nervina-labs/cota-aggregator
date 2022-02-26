41217c6d75ac7d1399e388e4d3098b625729adfb
duanyytop <duanyytop@gmail.com>
2022-02-26 14:52:58 +0800

    chore: Add response parser methods

--- src/response/claim.rs
+++ src/response/claim.rs
@@ -19,3 +19,14 @@ pub fn parse_claimed_smt(
     map.insert_u64("block_number", block_number);
     map
 }
+
+pub fn parse_claimed_update_smt(
+    (root_hash, smt_entry): (String, String),
+    block_number: u64,
+) -> Map<String, Value> {
+    let mut map = Map::new();
+    map.insert_str("smt_root_hash", root_hash);
+    map.insert_str("claim_update_smt_entry", smt_entry);
+    map.insert_u64("block_number", block_number);
+    map
+}
--- src/response/transfer.rs
+++ src/response/transfer.rs
@@ -12,3 +12,14 @@ pub fn parse_transfer_smt(
     map.insert_u64("block_number", block_number);
     map
 }
+
+pub fn parse_transfer_update_smt(
+    (root_hash, smt_entry): (String, String),
+    block_number: u64,
+) -> Map<String, Value> {
+    let mut map = Map::new();
+    map.insert_str("smt_root_hash", root_hash);
+    map.insert_str("transfer_update_smt_entry", smt_entry);
+    map.insert_u64("block_number", block_number);
+    map
+}
