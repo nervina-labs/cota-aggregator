4bec1de65e7c32cc58e556a8d81eacf77e606b22
duanyytop <duanyytop@gmail.com>
2022-06-16 14:40:17 +0800

    chore: Update testnet cota code hash

--- src/ckb/constants.rs
+++ src/ckb/constants.rs
@@ -1,4 +1,4 @@
 pub const TESTNET_COTA_CODE_HASH: &str =
-    "109cee8132469a37c194e9f391dfed96caf1b557ca85e604ec415f6da7a79308";
+    "89cd8003a0eaf8e65e0c31525b7d1d5c1becefd2ea75bb4cff87810ae37764d8";
 pub const MAINNET_COTA_CODE_HASH: &str =
     "1122a4fb54697cf2e6e3a96c9d80fd398a936559b90954c6e88eb7ba0cf652df";
