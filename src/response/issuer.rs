dda2f5916c91b3661260298f271110081bd63b62
duanyytop <duanyytop@gmail.com>
2022-09-28 15:53:50 +0800

    refactor: Update issuer info response

--- src/response/issuer.rs
+++ src/response/issuer.rs
@@ -1,27 +1,20 @@
 use crate::models::issuer::IssuerInfoDb;
 use crate::response::helper::Inserter;
+use crate::utils::error::Error;
 use jsonrpc_http_server::jsonrpc_core::serde_json::Map;
 use jsonrpc_http_server::jsonrpc_core::Value;
+
+use super::helper::parse_json_err;
 
 pub fn parse_issuer_response(
     issuer_info: Option<IssuerInfoDb>,
     block_number: u64,
-) -> Map<String, Value> {
-    let mut map = Map::new();
-    match issuer_info {
-        Some(issuer) => {
-            map.insert_str("name", issuer.name);
-            map.insert_str("avatar", issuer.avatar);
-            map.insert_str("description", issuer.description);
-        }
-        None => {
-            map.insert_null("name");
-            map.insert_null("avatar");
-            map.insert_null("description");
-        }
-    }
+) -> Result<Value, Error> {
+    let issuer = issuer_info.map_or(IssuerInfoDb::default(), |issuer| issuer);
+    let issuer_json = serde_json::to_string(&issuer).map_err(parse_json_err)?;
+    let mut map: Map<String, Value> = serde_json::from_str(&issuer_json).map_err(parse_json_err)?;
     map.insert_u64("block_number", block_number);
-    map
+    Ok(Value::Object(map))
 }
 
 pub fn parse_issuer_info_response(
