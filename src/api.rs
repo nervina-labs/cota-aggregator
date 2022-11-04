2d068013c21eac20fca8cf5091fb6b9992b1edee
duanyytop <duanyytop@gmail.com>
2022-11-04 16:45:45 +0800

    feat: Add generate_extension_subkey_smt rpc

--- src/api.rs
+++ src/api.rs
@@ -3,6 +3,7 @@ use crate::business::transaction::{get_cota_txs_by_block_number, get_history_tra
 use crate::entries::claim::generate_claim_smt;
 use crate::entries::claim_update::generate_claim_update_smt;
 use crate::entries::define::generate_define_smt;
+use crate::entries::extension::generate_ext_subkey_smt;
 use crate::entries::mint::generate_mint_smt;
 use crate::entries::subkey::generate_subkey_unlock_smt;
 use crate::entries::transfer::generate_transfer_smt;
@@ -19,6 +20,7 @@ use crate::models::issuer::get_issuer_info_by_lock_hash;
 use crate::models::joyid::get_joyid_info_by_lock_hash;
 use crate::request::claim::{ClaimReq, ClaimUpdateReq, IsClaimedReq};
 use crate::request::define::{DefineInfoReq, DefineReq};
+use crate::request::extension::ExtSubkeysReq;
 use crate::request::fetch::{
     FetchCcidInfoReq, FetchCountReq, FetchHistoryTxsReq, FetchIssuerInfoReq, FetchIssuerReq,
     FetchJoyIDReq, FetchReq, FetchTxsByBlockNumberReq,
@@ -32,6 +34,7 @@ use crate::request::witness::WitnessReq;
 use crate::response::ccid::parse_ccid_response;
 use crate::response::claim::{parse_claimed_response, parse_claimed_smt, parse_claimed_update_smt};
 use crate::response::define::{parse_define_info, parse_define_smt};
+use crate::response::extension::parse_extension_smt;
 use crate::response::hold::{parse_hold_response, parse_owned_nft_count};
 use crate::response::info::generate_aggregator_info;
 use crate::response::issuer::{parse_issuer_info_response, parse_issuer_response};
@@ -136,6 +139,16 @@ pub async fn subkey_unlock_rpc(params: Params, db: &RocksDB) -> Result<Value, Er
         .await
         .map_err(rpc_err)?;
     Ok(parse_subkey_unlock(subkey_smt, tip_number()?))
+}
+
+pub async fn extension_subkey_rpc(params: Params, db: &RocksDB) -> Result<Value, Error> {
+    info!("Extension subkey request: {:?}", params);
+    let map: Map<String, Value> = Params::parse(params)?;
+    let subkey_req = ExtSubkeysReq::from_map(&map).map_err(rpc_err)?;
+    let ext_subkey = generate_ext_subkey_smt(db, subkey_req)
+        .await
+        .map_err(rpc_err)?;
+    Ok(parse_extension_smt(ext_subkey, tip_number()?))
 }
 
 pub async fn fetch_hold_rpc(params: Params) -> Result<Value, Error> {
