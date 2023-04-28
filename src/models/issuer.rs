6640881431e17ab94cf8d3a5f086e98e0a006855
Dylan Duan <duanyytop@gmail.com>
2023-04-28 11:18:08 +0800

    feat: Add version and localization to issuer info

--- src/models/issuer.rs
+++ src/models/issuer.rs
@@ -1,5 +1,5 @@
 use crate::schema::issuer_infos::dsl::issuer_infos;
-use crate::schema::issuer_infos::{avatar, description, lock_hash, name};
+use crate::schema::issuer_infos::{avatar, description, localization, lock_hash, name, version};
 use crate::utils::error::Error;
 use diesel::*;
 use log::error;
@@ -9,15 +9,17 @@ use super::get_conn;
 
 #[derive(Serialize, Deserialize, Queryable, Debug, Clone, Eq, PartialEq, Default)]
 pub struct IssuerInfoDb {
-    pub name:        String,
-    pub avatar:      String,
-    pub description: String,
+    pub version:      String,
+    pub name:         String,
+    pub avatar:       String,
+    pub description:  String,
+    pub localization: String,
 }
 
 pub fn get_issuer_info_by_lock_hash(lock_hash_: [u8; 32]) -> Result<Option<IssuerInfoDb>, Error> {
     let lock_hash_hex = hex::encode(lock_hash_);
     let issuers: Vec<IssuerInfoDb> = issuer_infos
-        .select((name, avatar, description))
+        .select((version, name, avatar, description, localization))
         .filter(lock_hash.eq(lock_hash_hex))
         .limit(1)
         .load::<IssuerInfoDb>(&get_conn())
